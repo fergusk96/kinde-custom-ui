1a368b5d6feaecb9c8e9994766a49ee10ea9d638
ferg <ferguskavanagh1996@gmail.com>
2025-06-14 02:17:46 +0200

    validate failure reason

--- kindeSrc/environment/pages/(kinde)/(index)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(index)/page.tsx
@@ -1,14 +1,27 @@
 "use server";
 
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Widget } from "../../../../components/widget";
+import { DefaultLayout } from "../../../../layouts/default";
+import { Root } from "../../../../root";
+import { type KindePageEvent } from "@kinde/infrastructure";
+import React from "react";
+import { renderToString } from "react-dom/server.browser";
 
-export default function Page() {
-  const navigate = useNavigate();
+const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
+  return (
+    <Root context={context} request={request}>
+      <DefaultLayout>
+        <Widget
+          heading={context.widget.content.heading}
+          description={context.widget.content.description}
+        />
+      </DefaultLayout>
+    </Root>
+  );
+};
 
-  useEffect(() => {
-    window.location.replace("https://web.project-snake.win");
-  }, [navigate]);
-
-  return null;
+// Page Component
+export default async function Page(event: KindePageEvent): Promise<string> {
+  const page = await DefaultPage(event);
+  return renderToString(page);
 }
