d2c4c23bf12172f9c7591f50f1cd56afff581433
ferg <ferguskavanagh1996@gmail.com>
2025-06-14 02:22:54 +0200

    try another hack

--- kindeSrc/environment/pages/(kinde)/(index)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(index)/page.tsx
@@ -1,10 +1,30 @@
+"use server";
+
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Widget } from "../../../../components/widget";
+import { DefaultLayout } from "../../../../layouts/default";
+import { Root } from "../../../../root";
+import { type KindePageEvent } from "@kinde/infrastructure";
+import React from "react";
+import { renderToString } from "react-dom/server.browser";
 
-export default function Page() {
+const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
+  return (
+    <Root context={context} request={request}>
+      <DefaultLayout>
+        <Widget
+          heading='Welcome to the Default Page'
+          description='This is the default page of your Kinde application. You can customize it as needed.'
+        />
+      </DefaultLayout>
+    </Root>
+  );
+};
+
+// Page Component
+export default async function Page(event: KindePageEvent): Promise<string> {
   useEffect(() => {
-    window.location.replace("https://web.project-snake.win");
+    window.location.replace("https://web.project-snake.com");
   }, []);
-
-  return null;
+  return "";
 }
