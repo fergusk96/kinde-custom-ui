9484715344e9ba12cf2dc794a7f3b1ff035ddfea
ferg <ferguskavanagh1996@gmail.com>
2025-06-14 02:19:29 +0200

    retrying

--- kindeSrc/environment/pages/(kinde)/(index)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(index)/page.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Page() {
   useEffect(() => {
-    window.location.replace("https://web.project-snake.com");
+    window.location.replace("https://web.project-snake.win");
   }, []);
 
   return null;
