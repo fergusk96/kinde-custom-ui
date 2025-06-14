da6314216526368d195f24d9f3b69f53bbf62aa9
ferg <ferguskavanagh1996@gmail.com>
2025-06-14 02:18:43 +0200

    retrying

--- kindeSrc/environment/pages/(kinde)/(index)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(index)/page.tsx
@@ -1,27 +1,10 @@
-"use server";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Widget } from "../../../../components/widget";
-import { DefaultLayout } from "../../../../layouts/default";
-import { Root } from "../../../../root";
-import { type KindePageEvent } from "@kinde/infrastructure";
-import React from "react";
-import { renderToString } from "react-dom/server.browser";
+export default function Page() {
+  useEffect(() => {
+    window.location.replace("https://web.project-snake.com");
+  }, []);
 
-const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
-  return (
-    <Root context={context} request={request}>
-      <DefaultLayout>
-        <Widget
-          heading={context.widget.content.heading}
-          description={context.widget.content.description}
-        />
-      </DefaultLayout>
-    </Root>
-  );
-};
-
-// Page Component
-export default async function Page(event: KindePageEvent): Promise<string> {
-  const page = await DefaultPage(event);
-  return renderToString(page);
+  return null;
 }
