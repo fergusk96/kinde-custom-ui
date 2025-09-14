9cecbcfaa5dc1577f6fa773ae6e4979a0d286c47
Fergus Kavanagh <fergus.kavanagh@bitpanda.com>
2025-09-14 19:04:59 +0200

    add background image

--- kindeSrc/layouts/default.tsx
+++ kindeSrc/layouts/default.tsx
@@ -10,7 +10,9 @@ const styles: {
   },
   sidePanel: {
     borderRadius: "1rem",
-    backgroundColor: "dodgerblue",
+    backgroundImage: "url('https://web.project-snake.win/images/in_game_action-1024.webp')",
+    backgroundSize: "cover",
+    backgroundPosition: "center", 
     flex: 1,
     margin: "0.5rem",
     maxWidth: "1024px",
